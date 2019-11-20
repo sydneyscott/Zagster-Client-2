08e80721e0fd1f2a275b9ac50e7f72e7076c73d3
Sydney <scottsy@oregonstate.edu>
2019-11-20 15:47:44 -0800

    Graph

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-
-
-$(updateView)
-
-
+$(updateGraph)
 
 function updateView() {
-
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-
 }
 
 function updateRideCount(data) {
-
   numberOfRides = data.count
-
   $("h2#rideCount").html(numberOfRides)
+}
 
+function updateGraph(){
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 }
